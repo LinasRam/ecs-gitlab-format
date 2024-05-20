fcd9b3b135aeea08b9bda7b7e22d3a81e89921de
Linas Ramanauskas <l.ramanauskas@ba.lt>
2024-05-20 11:44:05 +0300

    Tests

--- tests/Console/Output/GitlabOutputFormatterTest.php
+++ tests/Console/Output/GitlabOutputFormatterTest.php
@@ -7,8 +7,10 @@ namespace LinasRam\EcsGitlabFormat\Tests\Console\Output;
 use LinasRam\EcsGitlabFormat\Console\Output\GitlabOutputFormatter;
 use PHPUnit\Framework\TestCase;
 use Symplify\EasyCodingStandard\Console\Style\EasyCodingStandardStyle;
+use Symplify\EasyCodingStandard\SniffRunner\ValueObject\Error\CodingStandardError;
 use Symplify\EasyCodingStandard\ValueObject\Configuration;
 use Symplify\EasyCodingStandard\ValueObject\Error\ErrorAndDiffResult;
+use Symplify\EasyCodingStandard\ValueObject\Error\FileDiff;
 
 /**
  * @covers \LinasRam\EcsGitlabFormat\Console\Output\GitlabOutputFormatter
@@ -27,22 +29,87 @@ final class GitlabOutputFormatterTest extends TestCase
 
     public function testReportGenerationWithErrors(): void
     {
+        $error = new CodingStandardError(
+            10,
+            'Error message',
+            'CheckerClass',
+            'path/file.php'
+        );
+        $fileDiff = new FileDiff(
+            'path/file.php',
+            'content',
+            'new content',
+            ['AnotherCheckerClass']
+        );
+
         $errorAndDiffResult = $this->createMock(ErrorAndDiffResult::class);
+        $errorAndDiffResult
+            ->expects($this->once())
+            ->method('getErrors')
+            ->willReturn([$error]);
+        $errorAndDiffResult
+            ->expects($this->once())
+            ->method('getFileDiffs')
+            ->willReturn([$fileDiff]);
         $configuration = $this->createMock(Configuration::class);
 
-        $this->easyCodingStandardStyle->expects($this->once())->method('writeln');
+        $expectedOutput = [
+            [
+                'type' => 'issue',
+                'check_name' => 'CheckerClass',
+                'description' => 'Error message',
+                'categories' => ['Style'],
+                'fingerprint' => md5('path/file.php' . 10 . 'CheckerClass'),
+                'severity' => 'major',
+                'location' => [
+                    'path' => 'path/file.php',
+                    'lines' => [
+                        'begin' => 10,
+                        'end' => 10,
+                    ],
+                ],
+            ],
+            [
+                'type' => 'issue',
+                'check_name' => 'AnotherCheckerClass',
+                'description' => 'AnotherCheckerClass',
+                'categories' => ['Style'],
+                'fingerprint' => md5('path/file.php' . 0 . 'AnotherCheckerClass'),
+                'severity' => 'minor',
+                'location' => [
+                    'path' => 'path/file.php',
+                    'lines' => [
+                        'begin' => 0,
+                        'end' => 0,
+                    ],
+                ],
+            ],
+        ];
+        $this->easyCodingStandardStyle
+            ->expects($this->once())
+            ->method('writeln')
+            ->with(json_encode($expectedOutput, \JSON_PRETTY_PRINT));
 
-        $this->gitlabOutputFormatter->report($errorAndDiffResult, $configuration);
+        $result = $this->gitlabOutputFormatter->report($errorAndDiffResult, $configuration);
+        $this->assertSame(1, $result);
     }
 
     public function testReportGenerationWithoutErrors(): void
     {
         $errorAndDiffResult = $this->createMock(ErrorAndDiffResult::class);
+        $errorAndDiffResult
+            ->expects($this->once())
+            ->method('getErrors')
+            ->willReturn([]);
         $configuration = $this->createMock(Configuration::class);
 
-        $this->easyCodingStandardStyle->expects($this->once())->method('writeln');
+        $this->easyCodingStandardStyle
+            ->expects($this->once())
+            ->method('writeln')
+            ->with(json_encode([], \JSON_PRETTY_PRINT));
 
-        $this->gitlabOutputFormatter->report($errorAndDiffResult, $configuration);
+        $result = $this->gitlabOutputFormatter->report($errorAndDiffResult, $configuration);
+        $this->assertSame(0, $result);
     }
 
     public function testGetName(): void
